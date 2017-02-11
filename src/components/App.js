e36d29cbd393c3b0961d20cc9636e89b67dbf91d
Roman Lykov <lykovrs@gmail.com>
2017-02-11 23:26:47 +0300

    add date range

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,29 @@
-import React, { PropTypes, Component } from 'react'
-import ArticleList from './ArticleList'
-import Chart from './Chart'
-import Select from 'react-select'
-import 'react-select/dist/react-select.css'
+import React, { PropTypes, Component } from 'react';
+import ArticleList from './ArticleList';
+import Chart from './Chart';
+import Range from './Range'
+import 'react-select/dist/react-select.css';
 
 class App extends Component {
     state = {
         user: '',
         selection: null
-    }
+    };
 
     render() {
         const {articles} = this.props
         const options = articles.map(article => ({
             label: article.title,
             value: article.id
-        }))
+        }));
         return (
             <div>
-                User: <input type="text" value={this.state.user} onChange={this.handleUserChange}/>
-                <Select options = {options} onChange={this.handleSelectChange} value={this.state.selection} multi/>
+                <Range />
                 <ArticleList articles={articles}/>
                 <Chart articles={articles}/>
             </div>
         )
-    }
+    };
 
     handleSelectChange = selection => this.setState({ selection })
 
@@ -32,13 +31,13 @@ class App extends Component {
         if (ev.target.value.length < 10) {
             this.setState({
                 user: ev.target.value
-            })
-        }
-    }
-}
+            });
+        };
+    };
+};
 
 App.propTypes = {
     articles: PropTypes.array.isRequired
-}
+};
 
-export default App
+export default App;
