03da0cb3a2a11bdf47437319f8bbe18f4f8ddbbd
Roman <kaboom.ua@gmail.com>
2017-02-13 11:51:45 +0200

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,6 @@
 import React, {Component,  PropTypes } from 'react';
 
+//лучше когда названия компонентов и файлов соответствуют
 class FormComment extends Component  {
     constructor(props) {
       super(props);
@@ -13,6 +14,7 @@ class FormComment extends Component  {
     };
 
     handleChange(event) {
+        //ок, но сделал бы уже const {name, value} = e.target тогда
       const target = event.target;
       const name = target.name;
       const value = target.value;
